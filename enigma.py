514c0f7de22bf8dd0bb62b8c1750f19b58b86fff
Ryan Cotsakis <ryancotsakis@gmail.com>
2018-04-09 00:33:14 -0700

    cleans up code with pycharm

--- enigma.py
+++ enigma.py
@@ -10,156 +10,149 @@ import time
 
 CLEAR_COMMAND = "cls"
 
-def encoder(text,pwNum):
-	binStr = ""
-	key = []
-	for bitNum in range(7*len(text)):
-		if pwNum%(((bitNum+1)%pwNum)+1) < ((bitNum+1)%pwNum)//2:
-			binStr += "1"
-		else:
-			binStr += "0"
-		if bitNum % 7 == 6:
-			key.append(int(binStr,2))
-			binStr = ""
-	newtext = ""
-	oldtext = ""
-	for x in range(len(text)):
-		charNum = ord(text[x])
-		if charNum >= 128:
-			charNum = ord("?")
-			oldtext += "?"
-		else:
-			oldtext += text[x]
-		newNum = charNum ^ key[x]
-		newtext += chr(newNum)
 
-	return oldtext, newtext
+def encoder(text, pw_num):
+    binary_string = ""
+    key = []
+    for bitNum in range(7*len(text)):
+        if pw_num % (((bitNum+1) % pw_num)+1) < ((bitNum+1) % pw_num) // 2:
+            binary_string += "1"
+        else:
+            binary_string += "0"
+        if bitNum % 7 == 6:
+            key.append(int(binary_string, 2))
+            binary_string = ""
+    new_text = ""
+    old_text = ""
+    for x in range(len(text)):
+        char_num = ord(text[x])
+        if char_num >= 128:
+            char_num = ord("?")
+            old_text += "?"
+        else:
+            old_text += text[x]
+        new_num = char_num ^ key[x]
+        new_text += chr(new_num)
 
-def write_to_file(file,text):
-	file.seek(0)
-	file.write(text)
-	file.truncate()
+    return old_text, new_text
+
+
+def write_to_file(f, text):
+    f.seek(0)
+    f.write(text)
+    f.truncate()
+
 
 def main():
-	print "Select a file, or press 'cancel' to create a new file.\n"
-	sys.stdout.flush()
-	root = Tkinter.Tk()
-	root.withdraw()
-	fname = tkFileDialog.askopenfilename(filetypes = (("Text Document","*.txt"),("Microsoft Word Document","*.docx"),("All Files","*.*")))
-	if fname == '':
-		print "Select a directory for the new file.\n"
-		sys.stdout.flush()
-		directory = tkFileDialog.askdirectory()
-		if directory == '':
-			return
-		version = 0
-		while True:
-			try:
-				f = open(directory + "/enigma_" + str(version) + ".txt", mode = "r")
-			except:
-				break
-			f.close()
-			version += 1
-		f = open(directory + "/enigma_" + str(version) + ".txt", mode = "w")
-		f.write("This file was automatically created by enigma.\nReplace this text with your information.")
-		f.close()
-		return
-	else:
-		f = codecs.open(fname, encoding = "utf-8", mode = "r+")
-		originalData = f.read()
+    print "Select a file, or press 'cancel' to create a new file.\n"
+    sys.stdout.flush()
+    root = Tkinter.Tk()
+    root.withdraw()
+    f_name = tkFileDialog.askopenfilename(filetypes=(("Text Document", "*.txt"), ("Microsoft Word Document", "*.docx"),
+                                                     ("All Files", "*.*")))
+    if f_name == '':
+        print "Select a directory for the new file.\n"
+        sys.stdout.flush()
+        directory = tkFileDialog.askdirectory()
+        if directory == '':
+            return
+        version = 0
+        while True:
+            try:
+                f = open(directory + "/enigma_" + str(version) + ".txt", mode="r")
+            except IOError:
+                break
+            f.close()
+            version += 1
+        f = open(directory + "/enigma_" + str(version) + ".txt", mode="w")
+        f.write("This file was automatically created by enigma.\nReplace this text with your information.")
+        f.close()
+        return
 
-		pw = getpass.getpass()
+    else:
+        f = codecs.open(f_name, encoding="utf-8", mode="r+")
+        original_data = f.read()
 
-		pwNum = 0
-		for char in pw:
-			pwNum *= 96
-			pwNum += ord(char)%96
+        pw = getpass.getpass()
 
-		originalData, newData = encoder(originalData,pwNum)
+        pw_num = 0
+        for char in pw:
+            pw_num *= 96
+            pw_num += ord(char) % 96
 
-		if not (pw in originalData or pw in newData):
-			print "WARNING: The password entered could not be found in the document"
-			sys.stdout.flush()
-			time.sleep(2)
+        original_data, new_data = encoder(original_data, pw_num)
 
+        if not (pw in original_data or pw in new_data):
+            print "WARNING: The password entered could not be found in the document"
+            sys.stdout.flush()
+            time.sleep(2)
 
+    print "\nCOMMAND LIST:"
+    print "write\t\t-translate text and overwrite the file"
+    print "view\t\t-translate text and print it to the screen"
+    print "current\t\t-view current text"
+    print "find\t\t-search for query in translated text"
+    print "add\t\t-append line of text to translated text"
+    print "password\t-print password"
+    print "clear\t\t-clear the output screen"
+    print "exit\t\t-close program"
 
-	print "\nCOMMAND LIST:"
-	print "write\t\t-translate text and overwrite the file"
-	print "view\t\t-translate text and print it to the screen"
-	print "current\t\t-view current text"
-	print "find\t\t-search for query in translated text"
-	print "add\t\t-append line of text to translated text"
-	print "password\t-print password"
-	print "clear\t\t-clear the output screen"
-	print "exit\t\t-close program"
+    while True:
+        print "\nEnter command:"
+        sys.stdout.flush()
 
-	while True: 
-		print "\nEnter command:"
-		sys.stdout.flush()
-		
-		command = raw_input().strip()
+        command = raw_input().strip()
 
-		if command in "write" and command[0] == "w":
-			write_to_file(f,newData)
-			newData, originalData = originalData, newData
-			print "Write successful."
+        if command in "write" and command[0] == "w":
+            write_to_file(f, new_data)
+            new_data, original_data = original_data, new_data
+            print "Write successful."
 
-		elif command in "exit" and command[0] == "e":
-			break
+        elif command in "exit" and command[0] == "e":
+            break
 
-		elif "find" in command or (command in "find" and command[0] == "f"):
-			if len(command) > 5:
-				query = command[5:].lower()
-			else:
-				print "Enter query:"
-				sys.stdout.flush()
-				query = raw_input().lower()
-			lines = newData.splitlines()
-			for line in lines:
-				if query in line.lower():
-					print line
+        elif "find" in command or (command in "find" and command[0] == "f"):
+            if len(command) > 5:
+                query = command[5:].lower()
+            else:
+                print "Enter query:"
+                sys.stdout.flush()
+                query = raw_input().lower()
+            lines = new_data.splitlines()
+            for line in lines:
+                if query in line.lower():
+                    print line
 
-		elif command in "add" and command[0] == "a":
-			print "Enter new line:"
-			sys.stdout.flush()
-			newLine = raw_input()
-			newData += "\n" + newLine
-			garbage, originalData = encoder(newData, pwNum)
-			write_to_file(f,originalData)
+        elif command in "add" and command[0] == "a":
+            print "Enter new line:"
+            sys.stdout.flush()
+            new_line = raw_input()
+            new_data += "\n" + new_line
+            garbage, original_data = encoder(new_data, pw_num)
+            write_to_file(f, original_data)
 
-		elif command in "view" and command[0] == "v":
-			print newData
+        elif command in "view" and command[0] == "v":
+            print new_data
 
-		elif command in "current" and command[:2] == "cu":
-			print originalData
+        elif command in "current" and command[:2] == "cu":
+            print original_data
 
-		elif command in "clear" and command[:2] == "cl":
-			os.system(CLEAR_COMMAND)
+        elif command in "clear" and command[:2] == "cl":
+            os.system(CLEAR_COMMAND)
 
-		elif command in "password" and command[0] == "p":
-			print pw
-			time.sleep(2)
-			os.system(CLEAR_COMMAND)
+        elif command in "password" and command[0] == "p":
+            print pw
+            time.sleep(2)
+            os.system(CLEAR_COMMAND)
 
-		else:
-			print "Invalid command, " + command
+        else:
+            print "Invalid command, " + command
 
-	try:
-		f.close()
-	except:
-		pass
+    try:
+        f.close()
+    except IOError:
+        pass
+
 
 if __name__ == '__main__':
-	main()
-
-
-
-
-
-
-
-
-
-
-
+    main()
