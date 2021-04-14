4b9ba73ca093b181f0304700c45bdff9252d7a04
Ryan Cotsakis <ryancotsakis@gmail.com>
2021-04-13 21:17:50 -0700

    changes to python3

--- enigma.py
+++ enigma.py
@@ -5,7 +5,7 @@ import os
 import getpass
 import sys
 
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 CLEAR_COMMAND = 'clear'
 
 
@@ -42,20 +42,20 @@ def write_to_file(f, text):
 
 
 def help():
-    print '\nCOMMAND LIST:'
-    print 'add []\t\t-append line of text to translated text'
-    print 'cat\t\t-view current text'
-    print 'clear\t\t-clear the output screen'
-    print 'find []\t\t-search for query in translated text'
-    print 'help\t\t-view this information'
-    print 'password\t-print password'
-    print 'q(uit)\t\t-close the program'
-    print 'write\t\t-translate text and overwrite the file\n'
+    print ('\nCOMMAND LIST:')
+    print ('add []\t\t-append line of text to translated text')
+    print ('cat\t\t-view current text')
+    print ('clear\t\t-clear the output screen')
+    print ('find []\t\t-search for query in translated text')
+    print ('help\t\t-view this information')
+    print ('password\t-print password')
+    print ('q(uit)\t\t-close the program')
+    print ('write\t\t-translate text and overwrite the file\n')
 
 
 def main(f_name=None):
     if f_name is None:
-        f_name = raw_input('Filename: ')
+        f_name = input('Filename: ')
 
     if f_name == '':
         return
@@ -66,7 +66,7 @@ def main(f_name=None):
     try:
         f = codecs.open(os.path.abspath(f_name), encoding='utf-8', mode='r+')
     except IOError:
-        print f_name + ': file not found'
+        print (f_name + ': file not found')
         return
 
     original_data = f.read()
@@ -84,19 +84,19 @@ def main(f_name=None):
     original_data, new_data = encoder(original_data, pw_num)
 
     if not (pw in original_data or pw in new_data):
-        print 'WARNING: the password entered could not be found in the document'
-        raw_input('Press <Enter> to continue')
+        print ('WARNING: the password entered could not be found in the document')
+        input('Press <Enter> to continue')
         f_name += '!'
 
     help()
 
     while True:
-        command = raw_input('enigma:' + f_name + '$ ').strip()
+        command = input('enigma:' + f_name + '$ ').strip()
 
         if command == 'write':
             write_to_file(f, new_data)
             new_data, original_data = original_data, new_data
-            print 'write successful'
+            print ('write successful')
 
         elif command == 'q' or command == 'quit':
             os.system(CLEAR_COMMAND)
@@ -106,23 +106,23 @@ def main(f_name=None):
             if len(command) > 5:
                 query = command[5:].lower()
             else:
-                query = raw_input('Search for: ').lower()
+                query = input('Search for: ').lower()
             lines = new_data.splitlines()
             for line in lines:
                 if query in line.lower():
-                    print line
+                    print (line)
 
         elif command[:3] == 'add':
             if len(command) > 4:
                 new_line = command[4:]
             else:
-                new_line = raw_input('New line: ')
+                new_line = input('New line: ')
             new_data += '\n' + new_line
             garbage, original_data = encoder(new_data, pw_num)
             write_to_file(f, original_data)
 
         elif command == 'cat':
-            print original_data
+            print (original_data)
 
         elif command == 'clear':
             os.system(CLEAR_COMMAND)
@@ -131,12 +131,12 @@ def main(f_name=None):
             help()
 
         elif command == 'password':
-            print pw
-            raw_input('Press <Enter> to continue')
+            print (pw)
+            input('Press <Enter> to continue')
             os.system(CLEAR_COMMAND)
 
         elif not command == '':
-            print command + ': command not found'
+            print (command + ': command not found')
 
     try:
         f.close()
@@ -148,12 +148,12 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         arg = sys.argv[1]
         if arg == '--help':
-            print 'enigma encrypts and decrypts sensitive documents\n'
-            print 'Usage:'
-            print 'enigma [filename]'
+            print ('enigma encrypts and decrypts sensitive documents\n')
+            print ('Usage:')
+            print ('enigma [filename]')
             help()
         elif arg == '--version':
-            print 'enigma ' + VERSION
+            print ('enigma ' + VERSION)
         else:
             main(arg)
     else:
